4686a5461a5cdf761f25dc7af4cd93e79e9b66d6
iyurchak <ivan.yurchak@outlook.com>
2025-10-02 10:07:44 +0200

    Solve 05_joinStrings

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -7,11 +7,17 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
+firstName = "Carlos";
+lastName = "Stevenson";
+thisYear = 1965;
+birthYear = 1947;
+
+fullName = `${firstName} ${lastName}`;
+age = thisYear - birthYear;
+
 // Add your code right below, good luck!
 
-
-
-
+greeting = `Hello! My name is ${fullName} and I am ${age} years old.`;
 
 // Do not change this
 module.exports = {
