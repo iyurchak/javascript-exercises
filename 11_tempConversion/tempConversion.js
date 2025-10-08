45e52369e34a943ea82337c7033681460733f310
iyurchak <ivan.yurchak@outlook.com>
2025-10-08 15:33:33 +0200

    Solve 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundOneDecimalPlaces = function(number) {
+  return Math.round((number + Number.EPSILON) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(temp) {
+  return roundOneDecimalPlaces((temp - 32) * 5 / 9);
+};
+
+const convertToFahrenheit = function(temp) {
+  return roundOneDecimalPlaces(temp * 9 / 5 + 32);
 };
 
 // Do not edit below this line
